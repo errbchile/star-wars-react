59b0ada690223eb5326c9985a738caf8b94960c3
errbchile <errbchile@gmail.com>
2024-06-22 21:41:38 +0200

    adding some tailwind

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,3 @@
 export default function HomePage() {
-  return <h1 className="text-3xl font-bold underline">HomePage</h1>;
+  return <div className="min-h-screen bg-black text-white">HomePage</div>;
 }
