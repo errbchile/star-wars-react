330b84c4a76b75014a61c72fce670b606e37df9b
errbchile <errbchile@gmail.com>
2024-06-23 13:46:49 +0200

    adding useAutoAnimate()

--- src/components/PersonList.jsx
+++ src/components/PersonList.jsx
@@ -1,13 +1,15 @@
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 import { useCharactersSelectedStore } from "../store/charactersSelectionStore";
 import PersonListItem from "./PersonListItem";
 
 export default function PersonList({ person }) {
   const { personList } = useCharactersSelectedStore();
+  const [animationParent] = useAutoAnimate();
 
   return (
     <>
       <h2 className={`text-center mb-2`}>Characters selected:</h2>
-      <ul className={`divide-y`}>
+      <ul className={`divide-y`} ref={animationParent}>
         {personList.map((person) => (
           <PersonListItem key={person.id} person={person} />
         ))}
