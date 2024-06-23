34eb7bab70ca33af2cf654d61a3453b1f0d37076
errbchile <errbchile@gmail.com>
2024-06-23 11:28:23 +0200

    persisting in localstorage

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,15 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useThemeStore = create((set) => ({
-  isDarkMode: false,
-  toggleIsDarkMode: () => set((state) => ({ isDarkMode: !state.isDarkMode })),
-}));
+export const useThemeStore = create(
+  persist(
+    (set) => ({
+      isDarkMode: false,
+      toggleIsDarkMode: () =>
+        set((state) => ({ isDarkMode: !state.isDarkMode })),
+    }),
+    {
+      name: "theme-storage",
+    }
+  )
+);
