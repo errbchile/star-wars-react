70b021b52c77bb4f3a548e6d2ae07ab24f75276a
errbchile <errbchile@gmail.com>
2024-06-23 10:33:25 +0200

    fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,6 @@
-import { useThemeStore } from "../store/store";
+import ThemeSwitch from "./ThemeSwitch";
 
 function Header() {
-  const { isDarkMode, toggleIsDarkMode } = useThemeStore();
-
   return (
     <header className="bg-gray-100 dark:bg-gray-800 p-4">
       <nav className="container mx-auto flex justify-between items-center px-8">
@@ -16,22 +14,7 @@ function Header() {
           <a href="/about" className="text-gray-900 dark:text-white">
             About
           </a>
-          <label
-            htmlFor="theme-toggle"
-            className="flex items-center cursor-pointer"
-          >
-            <div className="relative">
-              <input
-                id="theme-toggle"
-                type="checkbox"
-                className="sr-only"
-                checked={isDarkMode}
-                onChange={toggleIsDarkMode}
-              />
-              <div className="w-10 h-4 bg-gray-300 rounded-full shadow-inner dark:bg-gray-600"></div>
-              <div className="dot absolute w-6 h-6 bg-white rounded-full shadow -left-1 -top-1 transition transform dark:translate-x-full"></div>
-            </div>
-          </label>
+          <ThemeSwitch />
         </div>
       </nav>
     </header>
---
+++ src/components/ThemeSwitch.jsx
@@ -0,0 +1,21 @@
+import { useThemeStore } from "../store/store";
+
+export default function ThemeSwitch() {
+  const { isDarkMode, toggleIsDarkMode } = useThemeStore();
+
+  return (
+    <label htmlFor="theme-toggle" className="flex items-center cursor-pointer">
+      <div className="relative">
+        <input
+          id="theme-toggle"
+          type="checkbox"
+          className="sr-only"
+          checked={isDarkMode}
+          onChange={toggleIsDarkMode}
+        />
+        <div className="w-10 h-4 bg-gray-300 rounded-full shadow-inner dark:bg-gray-600"></div>
+        <div className="dot absolute w-6 h-6 bg-white rounded-full shadow -left-1 -top-1 transition transform dark:translate-x-full"></div>
+      </div>
+    </label>
+  );
+}
